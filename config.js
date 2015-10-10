a34a5087a4bd7deffc57a1e16718ed0c75e41bd0
Victor Nguyen <victorng1902@gmail.com>
2015-10-10 11:15:00 -0500

    remove config

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'key': 'utd-crss',
-    'database': 'postgres://%s:%s@sampleapollo.cccnh9pw1gdi.us-west-2.rds.amazonaws.com:5432/apollo',
-    'username': 'apoadmin',
-    'password': '165a22mkl74acs'
-};
