0482e6da1d754bdb679623d2ffe7bf658698813c
Victor Nguyen <victorng1902@gmail.com>
2015-11-29 23:44:56 -0600

    add story endpoint. Clean up some codes in moment and transcript endpoints

--- server.js
+++ server.js
@@ -43,119 +43,170 @@ app.use(apiRoute); // REGISTER ROUTES
 // ========================== ROUTES FOR OUR API ===============================
 // =============================================================================
 
+// *******************
+// ** Moment Viewer **
+// *******************
+
 // 1. Transcript
 apiRoute.route('/api/transcript')        
     .get(function(req,res){
         var result ={}; 
         var myTranscripts = [];   
-        var momentID, startTime, endTime;
-        
-        momentID = parseInt(req.query.momentID);
-        startTime = parseInt(req.query.startTime);
-        endTime = parseInt(req.query.endTime); 
+        var momentID, startTime, endTime; 
+        var momentIDParam,startTimeParam, endTimeParam;
 
+        momentIDParam = req.query.momentID;
+        startTimeParam = req.query.startTime;
+        endTimeParam = req.query.endTime;
 
-        if (momentID == "" || startTime == "" || endTime == "" || momentID == null || startTime == null || endTime == null )
+        if (momentIDParam == "" || startTimeParam == "" || endTimeParam == "" || momentIDParam == null || startTimeParam == null || endTimeParam == null )
             return res.json({"message" : "At least one param is empty or mising. Please try again"});
-            
+
+        momentID = parseInt(momentIDParam);
+        startTime = parseInt(startTimeParam);
+        endTime = parseInt(endTimeParam);
+
         // ===== query db ====
         pg.connect(conString, function(err, client, done) {
-            if (err) return res.json({"message" : "Error connecting to database"});
-            else {
-                
-                var queryString = sqlLib.SelectTranscript;
-                var temp_query = client.query(queryString,[momentID,startTime,endTime]);
+            if (err) return res.json({"message" : "Error connecting to database"});        
+            
+            var queryString = sqlLib.SelectAllTranscriptsWithinGivenInterval;
+            var temp_query = client.query(queryString,[momentID,startTime,endTime]);
 
-                // ===== processing returned data ====
-                temp_query.on("row",function(row){
-                    myTranscripts.push(row);        
-                });
-                // ===== processing returned data ====
+            // ===== processing returned data ====
+            temp_query.on("row",function(row){
+                myTranscripts.push(row);        
+            });
 
-                // ===== package data and send back to front-end ==== 
-                temp_query.on("end",function(){
-                    done();
-                    if (myTranscripts.length == 0) return res.json({"message" : "Incorrect metadata. Please try again"});
-                    else {
-                       result = {
-                            "message"       : "Success",
-                            "momentID"      : momentID,
-                            "startTime"     : startTime,
-                            "endTime"       : endTime,
-                            "transcripts"   : myTranscripts
-                        };  
-
-                        return res.json(result);          
-                    }    
-                    
-                });
-                // ===== package data and send back to front-end ==== 
-            }
+            // ===== package data and send back to front-end ==== 
+            temp_query.on("end",function(){
+                done();
+                if (myTranscripts.length == 0) return res.json({"message" : "Incorrect metadata. Please try again"});                
+                result = {
+                    "message"       : "Success",
+                    "momentID"      : momentID,
+                    "startTime"     : startTime,
+                    "endTime"       : endTime,
+                    "transcripts"   : myTranscripts
+                };
+                return res.json(result); 
+            });
+            // ===== package data and send back to front-end ==== 
         });
         // ===== query db ====
     });
 
-// 2. Moment - confirmed 1.0
+// 2. Moment 
 apiRoute.route('/api/moment') 
     .get(function(req,res){
         var momentID = req.query.momentID;
         if (momentID == null || momentID == "") 
             return res.json({ "message" : "Empty momentID. Please try again"});
-        else { 
-            var result ={}; 
-            var momentMetadata = []; // array of row objects
+         
+        var result ={}; 
+        var momentMetadata = []; // array of row objects
 
-            // ===== query db ====
-            pg.connect(conString, function(err, client, done){
-                if (err) return res.json({"message" : "Error connecting to database"});
-                else {
-                    var queryString = sqlLib.GenerateStreamingURL;
+        // ===== query db ====
+        pg.connect(conString, function(err, client, done){
+            if (err) return res.json({"message" : "Error connecting to database"});
+            
+            var queryString = sqlLib.GenerateStreamingURL;
+            var temp_query = client.query(queryString,[momentID]);
 
-                    var temp_query = client.query(queryString,[momentID]);
+            // ===== processing returned data ====
+            temp_query.on("row",function(row){
+                momentMetadata.push(row);        
+            });
 
-                    // ===== processing returned data ====
-                    temp_query.on("row",function(row){
-                        momentMetadata.push(row);        
+            // ===== package data and send back to front-end ====
+            temp_query.on("end",function(){
+                done();         
+                if (momentMetadata.length == 0) return res.json({"message" : "Incorrect momentID. Please try again"}); 
+
+                // cooking the audio url
+                var momentlength = momentMetadata[0].met_end - momentMetadata[0].met_start; // met-start and end are same for all rows so pick an arbitrary one and extract these two
+                var momentstart = momentMetadata[0].met_start;
+                // right now its only mission 11 - might need to query/change above query to get mission id
+                var audioURL  = config.defaultStreamingURL;
+                for (var i = 0; i < momentMetadata.length; i++){
+                    audioURL += "&channel=" + momentMetadata[i].channel_id         
+                }
+                audioURL += "&format=m4a"; // default format for now?
+                audioURL += "&t=" + momentstart;
+                audioURL += "&len=" + momentlength;    
+                //console.log("Audio url " + audioURL); 
+
+                // body?
+                // upcoming moments?
+
+                // package result object
+                result = {
+                    "message"         : "Success",
+                    "title"           : momentMetadata[0].title,
+                    "audioURL"        : audioURL,
+                    "startTime"       : momentstart,
+                    "length"          : momentlength,
+                    "body"            : {}
+                };
+                return res.json(result); 
+                
+            });        
+              
+        });  // end outter query        
+        // ===== query db ====
+    });
+
+// 3. Body???
+
+// *******************
+// ** Story Viewer **
+// *******************
+
+// 1. Story
+apiRoute.route('/api/story')        
+    .get(function(req,res){
+        var storyID = req.query.storyID;
+        if (storyID == null || storyID == "") 
+            return res.json({ "message" : "Empty storyID. Please try again"});     
+        var result ={}; 
+        var momentList = [];
+        var tempArray = []; // storing raw rows returned
+
+        // ===== query db ====
+        pg.connect(conString, function(err, client, done){
+            if (err) return res.json({"message" : "Error connecting to database"});
+            var queryString = sqlLib.SelectAllMomentsOfGivenStory;
+            var temp_query = client.query(queryString,[storyID]);
+
+            // ===== processing returned data ====
+            temp_query.on("row",function(row){
+                tempArray.push(row);        
+            });
+
+            // ===== package data and send back to front-end ====
+            temp_query.on("end",function(){
+                done();         
+                if (tempArray.length == 0) return res.json({"message" : "Incorrect storyID. Please try again"});
+                for (var i =0; i<tempArray.length; i++){
+                    momentList.push({
+                        "id"            : tempArray[i].moment_id,
+                        "title"         : tempArray[i].moment_title,
+                        "order"         : tempArray[i].moment_order,
+                        "met_start"     : tempArray[i].met_start,
+                        "met_end"       : tempArray[i].met_end,
+                        "description"   : tempArray[i].moment_description
                     });
-                    // ===== processing returned data ====
-                    temp_query.on("end",function(){
-                        done();         
-                        if (momentMetadata.length == 0) return res.json({"message" : "Incorrect momentID. Please try again"}); 
-                        else { // valid momentID
-
-                            // cooking the audio url
-                            var momentlength = momentMetadata[0].met_end - momentMetadata[0].met_start; // met-start and end are same for all rows so pick an arbitrary one and extract these two
-                            var momentstart = momentMetadata[0].met_start;
-                            // right now its only mission 11 - might need to query/change above query to get mission id
-                            var audioURL  = config.defaultStreamingURL;
-                            for (var i = 0; i < momentMetadata.length; i++){
-                                audioURL += "&channel=" + momentMetadata[i].channel_id         
-                            }
-                            audioURL += "&format=m4a"; // default format for now?
-                            audioURL += "&t=" + momentstart;
-                            audioURL += "&len=" + momentlength;    
-                            //console.log("Audio url " + audioURL); 
-
-                            // body?
-                            // upcoming moments?
-
-                            // package result object
-                            result = {
-                                "message"         : "Success",
-                                "title"           : momentMetadata[0].title,
-                                "audioURL"        : audioURL,
-                                "startTime"       : momentstart,
-                                "length"          : momentlength,
-                                "body"            : {}
-                            };
-                            return res.json(result); 
-                        }
-                    });        
-                }  
-            });  // end outter query        
-            // ===== query db ====
-
-        }    
+                }
+                result = {
+                    "message"         : "Success",
+                    "id"              : storyID,
+                    "title"           : tempArray[0].story_title,
+                    "description"     : tempArray[0].story_description,  
+                    "momentList"      : momentList
+                };
+                return res.json(result); 
+            });
+        });
     });
 
 // =============================================================================
--- sqlLib.js
+++ sqlLib.js
@@ -4,7 +4,8 @@
 
 
 // be cautious for transcripts close to the boundaries
-var SelectTranscript = "SELECT tmp.id, tmp.message, tmp.met_start, tmp.met_end, tmp.channel_id, tmp.speaker_id, speakers.name\
+var SelectAllTranscriptsWithinGivenInterval = 
+                        "SELECT tmp.id, tmp.message, tmp.met_start, tmp.met_end, tmp.channel_id, tmp.speaker_id, speakers.name\
                             FROM\
                             (\
                                 (\
@@ -16,13 +17,31 @@ var SelectTranscript = "SELECT tmp.id, tmp.message, tmp.met_start, tmp.met_end, 
                                 INNER JOIN speakers ON tmp.speaker_id = speakers.id\
                             ) ORDER BY tmp.met_start ASC"; 
 
-var GenerateStreamingURL = "SELECT tmp.moment_id, tmp.channel_id, moments.title, moments.met_start, moments.met_end\
+var GenerateStreamingURL = 
+                    "SELECT tmp.moment_id, tmp.channel_id, moments.title, moments.met_start, moments.met_end\
                         FROM(\
                             (\
                                 SELECT moment_id, channel_id FROM moment_channel_join WHERE moment_id = $1\
                             ) AS tmp\
                             INNER JOIN moments ON moments.id = tmp.moment_id)\
                         ORDER BY tmp.channel_id ASC";
+var SelectAllMomentsOfGivenStory = 
+                   "SELECT tmp2.story_id, stories.title as story_title, stories.description as story_description,\
+                           tmp2.moment_id, tmp2.title as moment_title, tmp2.moment_order, tmp2.met_start,\
+                           tmp2.met_end, tmp2.description as moment_description\
+                        FROM\
+                          (\
+                            (\
+                                (\
+                                   SELECT story_id, moment_id, moment_order\
+                                   FROM moment_story_join WHERE story_id = $1\
+                                ) AS tmp\
+                                INNER JOIN moments ON tmp.moment_id = moments.ID\
+                            ) AS tmp2\
+                            INNER JOIN stories ON tmp2.story_id = stories.id\
+                          )\
+                        ORDER BY tmp2.moment_order ASC";
 
-exports.SelectTranscript = SelectTranscript;
+exports.SelectAllTranscriptsWithinGivenInterval = SelectAllTranscriptsWithinGivenInterval;
 exports.GenerateStreamingURL = GenerateStreamingURL;
+exports.SelectAllMomentsOfGivenStory = SelectAllMomentsOfGivenStory;
