47816c5871a55bdd20406e93b6c672c50f2816f2
Cole Dean Shepherd <cole@coledeanshepherd.com>
2016-11-11 10:21:12 -0600

    Added a route and controller method for getting a random moment. (#51)
    * Added a route and controller method for getting a random moment.

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,9 @@ Rails.application.routes.draw do
       member do
         get :audio, to: :audio
       end
+      collection do
+        get "random"
+      end
 	  end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
