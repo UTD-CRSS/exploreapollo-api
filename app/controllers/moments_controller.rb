5f058097ec8f27eea9127cb842e6d531faf7173a
Mark Yanaros <mark.yanaros@gmail.com>
2016-12-08 15:48:06 -0600

    my - moment search looks at moments title and description

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -51,7 +51,7 @@ class MomentsController < ApplicationController
   end
 
   def search
-    render json: Moment.ransack(text_of_transcript_items_of_channels: params[:q]).result, each_serializer: MomentShortSerializer
+    render json: Moment.ransack(title_or_description_cont: params[:q]).result, each_serializer: MomentShortSerializer
   end
 
   private
